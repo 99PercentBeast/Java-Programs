bbc0edf3185c671c26fbc70bf35c4cd0332809be
Aasim-Linux <Aasimbaig01@gmail.com>
2017-12-08 20:16:29 +0530

    FibonacciRecursive added

---
+++ src/FibonacciRecursion.java
@@ -0,0 +1,14 @@
+//Fibonacci using recursion
+import java.util.Scanner;
+public class FibonacciRecursion {
+    static int fibo(int number){
+        if (number<=1) return number;
+        else return((fibo(number-1))+fibo(number-2));
+    }
+    public static void main(String args[]){
+        System.out.print("Enter a number for fibonacci :");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.print(fibo(number));
+    }
+}
